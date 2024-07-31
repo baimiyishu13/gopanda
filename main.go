61b35c7654498cff9958b58d3acfddd9af873a93
jskt6155 <lele.ma-vendor@yumchina.com>
2024-07-31 11:53:51 +0800

    chi

--- main.go
+++ main.go
@@ -6,17 +6,35 @@ import (
 	"net/http"
 )
 
-func handlerfunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	// index.html
-	http.ServeFile(w, r, "index.html")
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	// contact.html
+	http.ServeFile(w, r, "./static/contact.html")
+}
+
+// 404
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	// 404.html
+	http.ServeFile(w, r, "./static/404.html")
+
 }
 
 func main() {
 	// 初始化 logrus
 	log := logger.GetLogger()
-	// chi 路由器
+	//chi 路由器
 	r := chi.NewRouter()
-	r.Get("/", handlerfunc)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	// 404
+	r.NotFound(notFoundHandler)
 
 	// 启动服务器
 	log.Info("starting server on :3000")
